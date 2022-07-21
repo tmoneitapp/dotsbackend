c712386bfbb1e2c504c71e304083a46146a42adc
kengwoh <lee.kengwoh@tm.com.my>
2022-07-21 23:38:24 +0800

    Added Order API v21

--- routes/dashboard.js
+++ routes/dashboard.js
@@ -9,6 +9,13 @@ var dashboardRouter = express.Router();
 dashboardRouter.use(bodyParser.json());
 
 dashboardRouter.get('/', authenticate.authenticateToken, (req, res, next) =>{
-    res.sendStatus(403);
+    //res.sendStatus(403);
+    res.statusCode = 403;
+    res.setHeader('Content-Type','application/json');
+    //res.end(password_hash);
+    res.end('no result return');
 });
+
+module.exports = dashboardRouter;
+
 
