280c592b094851f05705bb6555bb79360bb9c2c5
kengwoh <lee.kengwoh@tm.com.my>
2022-07-26 16:32:30 +0800

    Amend User API v28

--- services/db.js
+++ services/db.js
@@ -5,7 +5,7 @@ async function query(sql, params) {
   const connection = await mysql.createConnection(config.db);
   const [results, ] = await connection.execute(sql, params);
 
-  connection.release(); //added by kengwoh
+  //connection.release(); //added by kengwoh
   return results;
 }
 
