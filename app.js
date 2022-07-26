37a36753d403a2dfdd7aefc8064e620cdeb28dcd
kengwoh <lee.kengwoh@tm.com.my>
2022-07-26 20:56:57 +0800

    Amend User API v31

--- app.js
+++ app.js
@@ -35,8 +35,8 @@ app.set('view engine', 'jade');
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
-app.use(passport.initialize());
-//app.use(cookieParser());
+//app.use(passport.initialize());
+app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
