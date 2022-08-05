4f40c9884d7aea1841e5617721ca1d1cc1f23bcc
kengwoh <lee.kengwoh@tm.com.my>
2022-08-05 15:28:03 +0800

    Update app.js

--- app.js
+++ app.js
@@ -1,14 +1,12 @@
-var createError = require('http-errors');
-var express = require('express');
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
-var cors = require('cors');
-var passport = require('passport');
-
+var createError = require("http-errors");
+var express = require("express");
+var path = require("path");
+var cookieParser = require("cookie-parser");
+var logger = require("morgan");
+var cors = require("cors");
+var passport = require("passport");
 
 //import { swaggerDocument } from './swagger';
-
 
 var app = express();
 app.use(cors());
@@ -21,70 +19,75 @@ app.use(cors());
 //   }
 // });
 
-app.set('base','/api');
+app.set("base", "/api");
 // view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'jade');
+app.set("views", path.join(__dirname, "views"));
+app.set("view engine", "jade");
 
-app.use(logger('dev'));
+app.use(logger("dev"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 //app.use(passport.initialize());
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, "public")));
 
+var indexRouter = require("./routes/index");
+app.use("/api", indexRouter);
 
-var indexRouter = require('./routes/index');
-app.use('/api', indexRouter);
+var usersRouter = require("./routes/users");
+app.use("/api/users", usersRouter);
 
-var usersRouter = require('./routes/users');
-app.use('/api/users', usersRouter);
+var ordersRouter = require("./routes/orders");
+app.use("/api/orders", ordersRouter);
 
-var ordersRouter = require('./routes/orders');
-app.use('/api/orders',ordersRouter);
+var dashboardRouter = require("./routes/dashboard");
+app.use("/api/dashboard", dashboardRouter);
 
-var dashboardRouter = require('./routes/dashboard');
-app.use('/api/dashboard', dashboardRouter);
+var cdyRouter = require("./routes/cdy");
+app.use("/api/cdys", cdyRouter);
 
-var cdyRouter = require('./routes/cdy');
-app.use('/api/cdys',cdyRouter);
+var pmRouter = require("./routes/pm");
+app.use("/api/pms", pmRouter);
 
-var pmRouter = require('./routes/pm');
-app.use('/api/pms',pmRouter);
+var orderTypeRouter = require("./routes/ordertype");
+app.use("/api/ordertypes", orderTypeRouter);
 
-var orderTypeRouter = require('./routes/ordertype');
-app.use('/api/ordertypes', orderTypeRouter);
+var productTypeRouter = require("./routes/producttype");
+app.use("/api/producttypes", productTypeRouter);
 
-var productTypeRouter = require('./routes/producttype');
-app.use('/api/producttypes', productTypeRouter);
+var dropDownRouter = require("./routes/dropdown");
+app.use("/api/dropdown", dropDownRouter);
 
-var dropDownRouter = require('./routes/dropdown');
-app.use('/api/dropdown', dropDownRouter);
+var authorityRouter = require("./routes/authority");
+app.use("/api/authorities", authorityRouter);
 
-var authorityRouter = require('./routes/authority');
-app.use('/api/authorities', authorityRouter);
+var roleRouter = require("./routes/roles");
+app.use("/api/roles", roleRouter);
 
-var roleRouter = require('./routes/roles');
-app.use('/api/roles', roleRouter);
+var resourceRouter = require("./routes/resources");
+app.use("/api/resources", resourceRouter);
 
-var swaggerUi = require('swagger-ui-express'),
-  swaggerDocument = require('./swagger.json');
-app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
+var orderSdRouter = require("./routes/order_sd");
+app.use("/api/order_sd", orderSdRouter);
+
+var swaggerUi = require("swagger-ui-express"),
+  swaggerDocument = require("./swagger.json");
+app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 
 // catch 404 and forward to error handler
-app.use(function(req, res, next) {
+app.use(function (req, res, next) {
   next(createError(404));
 });
 
 // error handler
-app.use(function(err, req, res, next) {
+app.use(function (err, req, res, next) {
   // set locals, only providing error in development
   res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
+  res.locals.error = req.app.get("env") === "development" ? err : {};
 
   // render the error page
   res.status(err.status || 500);
-  res.render('error');
+  res.render("error");
 });
 
 module.exports = app;
